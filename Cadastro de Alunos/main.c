202befb174187e547cd177860143ef5815586b98
Felipe Tavares <xfelipetavares@gmail.com>
2021-06-02 13:18:12 -0300

    Initial Commit

---
+++ Cadastro de Alunos/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Cadastro de Alunos
+//
+//  Created by Felipe Tavares on 02/06/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
